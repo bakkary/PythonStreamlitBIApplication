b55c1f653c4d643e9d32fa0c0a21d2c443344996
Chris Hedin Ziegler <60019587+ChriZZZy@users.noreply.github.com>
2024-11-20 10:13:34 +0100

    changed hpmepage streamlit

--- HOP-ReactStreamlit-Application/src/graphs.jsx
+++ HOP-ReactStreamlit-Application/src/graphs.jsx
@@ -10,24 +10,8 @@ function GraphsPage() {
     <div className="userflex">
       <div className="user-flex-item flex-item2">
         <div className="diary">
-          <div className="diary-items diary-item1">
-            <form>
-              <input type="text" name="date" placeholder="Date" className="UP1UP3" />
-            </form>
-          </div>
-          <div className="diary-items diary-item2">
-            <form>
-              <input type="text" name="mood" placeholder="Pick your mood" className="UP2" />
-            </form>
-          </div>
-          <div className="diary-items diary-item3">
-            <form>
-              <input type="text" name="topic" placeholder="Today's topic" className="UP1UP3" />
-            </form>
-          </div>
           <div className="diary-items diary-item4">
             <label htmlFor="streamlitApp">Streamlit Application</label>
-            
               <iframe
               className="diary-box"
               id="streamlitApp"
